8b7bbf4d2c25df5ca6b263901d7a06e228be6745
Nikita Krasnov <nikitosikrasnov@gmail.com>
2023-05-26 21:26:41 +0300

    model

---
+++ monkeys/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.utils.safestring import mark_safe
+from django_cleanup.signals import cleanup_pre_delete
+from sorl.thumbnail import delete, get_thumbnail
+
+
+class Monkey(models.Model):
+    title = models.IntegerField()
+    image = models.ImageField(
+        upload_to='images/%Y/%m',
+        verbose_name="image",
+        help_text='load image'
+    )
+
+    @property
+    def get_img(self):
+        return get_thumbnail(self.upload, '300x300', crop='center', quality=51)
+
+    def img_tmb(self):
+        if self.upload:
+            return mark_safe(
+                f'<img src="{self.get_img.url}">'
+            )
+        return 'no images'
+
+    img_tmb.short_description = 'preview'
+    img_tmb.allow_tags = True
+
+    def sorl_delete(**kwargs):
+        delete(kwargs['file'])
+
+    cleanup_pre_delete.connect(sorl_delete)
+
+    def __str__(self):
+        return self.upload.url
+
+    class Meta:
+        verbose_name = 'monkey'
+        verbose_name_plural = 'monkeys'
