798e271daf9f5206044fc38c35b5c038fd7d0cc8
Nikita Krasnov <nikitosikrasnov@gmail.com>
2023-07-16 17:10:04 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ pip3 install -U pip
 pip3 install -U setuptools
 pip install -r requirements.txt
 python3.9 makemigrations monkeys contacts api
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput
